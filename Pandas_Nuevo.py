b48d2ecc20ab0d9b41d78d37911b0475519d9c68
XavZol <alansolis707@gmail.com>
2026-04-02 21:14:18 -0600

    Fusionar DataFrames con Merge

--- Pandas_Nuevo.py
+++ Pandas_Nuevo.py
@@ -1,25 +1,19 @@
 import pandas as pd
 import json as json
 
+df1 = pd.DataFrame({'ID': [1, 2, 3],
+                    'Nombre': ['Ana', 'Luis', 'Carlos'] })
+df2 = pd.DataFrame({'ID': [1, 2, 4], 
+                    'Edad': [25, 30, 23]})
 
-ruta = r'C:\Users\javie\OneDrive\Desktop\Top-Películas.csv'
-df = pd.read_csv(ruta)
+print(df1)
+print(df2)
 
-print(df)
+df_combinado = pd.merge(df1, df2, on='ID' , how='outer')
+print(df_combinado)
 
-df_ordenado = df.sort_values(by='rating', ascending=False)
-df_ordenado.head(10)
-print(df)
+df_combinado = pd.merge(df1, df2, on='ID' , how='right')
+print(df_combinado)
 
-
-df_ordenado = df.sort_values(by=['rating', 'recaudación(M)'], ascending=False)
-df_ordenado.head(10)
-print(df)
-
-df_agrupado = df.groupby('género')['rating'].mean()
-print(df_agrupado)
-
-
-df_agrupado = df.groupby('año')['recaudación(M)'].sum()
-df_agrupado.sort_values(ascending=False).head()
-print(df_agrupado)
+df_indexado = pd.merge(df1, df2, left_index=True, right_index=True)
+print(df_indexado)
